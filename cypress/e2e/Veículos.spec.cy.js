d95ed1f19fb5e6de645d722a136362c3596b5e79
Guilherme-Aragao <guilhermearagao2001@hotmail.com>
2023-09-15 15:25:56 -0300

    commit veiculos

--- cypress/e2e/Veículos.spec.cy.js
+++ cypress/e2e/Veículos.spec.cy.js
@@ -1,1 +1,22 @@
-qualquer coisa ai 
+/// <reference types = "cypress"/>
+
+beforeEach(() => {
+    cy.visit('https://backoffice-stg.mobfleet.com.br');
+    cy.get('#mui-1').type('administrador@4code.dev')
+    cy.get('#mui-2').type('Default2012*')
+    cy.get('[data-cy="submit"]').click()
+})
+
+context('Funcionalidade Login', () =>{
+
+    it('Cadastrar Veículo', () => {
+    cy.get('[class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-1ag30nc"]').click()
+    cy.get('[href="/vehicles"]').click()
+    cy.get('[data-cy="entityCreateButton"]').click()
+    cy.get('#vehicle-licensePlate').type('dsfdsfdsf')
+    cy.get('#vehicle-chassis').type('ajdnfiuwenf')
+    cy.get('#vehicle-renavam').type('kdfnisdnfik')
+    cy.get('#mui-17').click().select('[class="MuiMenuItem-root MuiMenuItem-gutters MuiButtonBase-root css-1x7oc0z"]')
+
+    });
+})
